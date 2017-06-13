40a8b18d10f5e10805c0102c4fb9a8e8ccc812da
Thijs Deelen <deelenthijs@gmail.com>
2017-06-13 14:02:43 +0200

    body parser verwijderd

--- VideoServer/server.js
+++ VideoServer/server.js
@@ -9,12 +9,6 @@ var exampleObject = {
 	farewell: "Goodbye world..."
 }
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
 app.set('port', (process.env.PORT || 5000))
 
 // Gets.
