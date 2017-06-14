e18e319b7d49902e196bc62645b79671145617a2
Bram van de Griend <shep.ukn@gmail.com>
2017-06-14 13:43:22 +0200

    Request logger en 404 catch added.

--- VideoServer/server.js
+++ VideoServer/server.js
@@ -1,7 +1,11 @@
 // Initializeren.
-var express = require('express')
-var app = express()
+var http = require('http');
+var express = require('express');
 var mysql = require('mysql');
+var dateTime = require('node-datetime');
+
+
+var app = express();
 
 var connection = mysql.createConnection({
   host: "146.185.130.82",
@@ -27,7 +31,21 @@ var exampleObject =
 // Port.
 app.set("port", (process.env.PORT || 5000))
 
+
+
+
+
+
 // Gets.
+
+// Display all requests.
+app.all('*', function(request, response, next) {
+	var dt = dateTime.create();
+	var formatted = dt.format('H:M:S');
+	console.log("[" + formatted + "] " + request.method + " " + request.url);
+	next();
+})
+
 app.get('/', function(request, response) {
 	response.send("[Success] - Hello Avans!")
 })
@@ -36,10 +54,13 @@ app.get('/about', function(request, response) {
 	response.send("[Success] - Dit is een webserver voor de toets van Programmeren 4. Gemaakt door Bram van de Griend en Thijs Deelen.")
 })
 
+// 404 response.
 app.all('*', function(request, response) {
 	response.status(404);
 	response.send("[Failed] - Item niet gevonden.");
 })
+	
+
 
 // Start server.
 app.listen(app.get('port'), function() {
