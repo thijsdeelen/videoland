84d3ab3040faeb53b9e376e66e0f13efe10f42fc
Bram van de Griend <shep.ukn@gmail.com>
2017-06-14 15:21:06 +0200

    CRUD functionaliteit toegevoegd aan de API.

--- VideoServer/api/crud.js
+++ VideoServer/api/crud.js
@@ -2,22 +2,57 @@ var express = require('express');
 var path = require('path');
 var routes = express.Router();
 
+// SQL!
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host: "146.185.130.82",
+  user: "1013",
+  password: process.env.DB_PASSWORD,
+  database: "1013"
+});
 
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("[Success] - Connected to database.");
+});
+
+// CRUD!
 routes.get('/create', function(request, response){
-	response.send("ik ben een create");
+	var query = "INSERT INTO `film` VALUES (NULL, 'Documentaire Avans Hogeschool', 'Een docu over Avans Hogeschool.', '2017', '', NULL, '3', '4.99', '56', '19.99', 'G', NULL, CURRENT_TIMESTAMP)";
+	connection.query(query, function (err, results, fields) {
+        if (err) throw err;
+        console.log("[CREATE] - Film created.");
+        response.send(results);
+    });
+});
+
+routes.get('/read', function(request, response){
+	var query = "SELECT * FROM film WHERE title LIKE '%Avans%' LIMIT 10";
+	connection.query(query, function (err, results, fields) {
+        if (err) throw err;
+        console.log("[READ] - Film read.");
+        response.send(results);
+    });
 });
 
 routes.get('/update', function(request, response){
-	res.contentType('application/json');
-	res.status(200);
-	res.json(mijnObject);
+	var query = "UPDATE film SET title = 'Docu-Docu van de coole Avans school' WHERE title LIKE '%Avans%'";
+	connection.query(query, function (err, results, fields) {
+        if (err) throw err;
+        console.log("[UPDATE] - Film updated.");
+        response.send(results);
+    });
 });
 
 routes.get('/delete', function(request, response){
-	res.contentType('application/json');
-	res.status(200);
-	res.json(mijnObject);
+	var query = "DELETE FROM film WHERE title LIKE '%Avans%'";
+	connection.query(query, function (err, results, fields) {
+        if (err) throw err;
+        console.log("[DELETE] - Film deleted.");
+        response.send(results);
+    });
 });
 
+// End.
 module.exports = routes;
--- VideoServer/server.js
+++ VideoServer/server.js
@@ -1,42 +1,11 @@
 // Initializeren.
 var http = require('http');
 var express = require('express');
-var mysql = require('mysql');
 var dateTime = require('node-datetime');
-
-
 var app = express();
-
-var connection = mysql.createConnection({
-  host: "146.185.130.82",
-  user: "1013",
-  password: process.env.DB_PASSWORD,
-  database: "1013"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("[Success] - Connected to database.");
-});
-
-
-
-// Objecten.
-var exampleObject =
-{
-	greeting: "Hello world!",
-	farewell: "Goodbye world..."
-}
 
 // Port.
 app.set("port", (process.env.PORT || 5000))
-
-
-
-
-
-
-// Gets.
 
 // Display all requests.
 app.all('*', function(request, response, next) {
@@ -61,8 +30,6 @@ app.all('*', function(request, response) {
 	response.status(404);
 	response.send("[Failed] - Item niet gevonden.");
 })
-
-
 
 // Start server.
 app.listen(app.get('port'), function() {
